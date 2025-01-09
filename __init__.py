4e5dc70f4e66711b615a3a2d04b45e0b1a91a2d5
Mini <alexis1920041@gmail.com>
2025-01-09 10:12:49 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,9 +7,13 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
+@app.route('Contact')
+def hello_world():
+    return render_template('Ma page de contact')
+
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #commentaire
+    return render_template('hello.html')
   
 if __name__ == "__main__":
   app.run(debug=True)
